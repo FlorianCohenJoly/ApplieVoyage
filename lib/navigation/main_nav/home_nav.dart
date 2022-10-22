e1330d968d7a545e4c1f470a76e721652e23a82c
FlorianCohenJoly <floriancohenjoy@gmail.com>
2022-10-23 01:57:10 +0200

    Update home_nav.dart

--- lib/navigation/main_nav/home_nav.dart
+++ lib/navigation/main_nav/home_nav.dart
@@ -1,4 +1,3 @@
-import 'package:appli_voyage/intro_screens/main_screen_info.dart';
 import 'package:flutter/material.dart';
 
 
@@ -14,7 +13,8 @@ class _HomeNavState extends State<HomeNav> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MainPage(),
+      body: Text("Home"),
+      backgroundColor: Colors.green.shade400
     );
   }
 }
